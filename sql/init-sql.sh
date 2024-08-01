55449372b91176520ba0b2b94b427836cfa0bcc6
daihao_4371 <daihao_4371@163.com>
2024-08-01 17:50:19 +0800

    初始化SQL脚本

---
+++ sql/init-sql.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# 初始化数据库
+user="root"
+password="123456"
+db="cloudops"
+prot="3306"
+host="127.0.0.1"
+
+# 创建数据库
+mysql -u$user -p$password -h$host -P$prot <<EOF
+CREATE DATABASE IF NOT EXISTS $db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+EOF
+# 导入数据
+mysql -u$user -p$password -h$host -P$prot $db < /cloudops/sql/init.sql
